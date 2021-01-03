37e81fc431c2626e49f6aa4404319420055019e5
Keurig-Creator <threetwoone112@gmail.com>
2021-01-02 23:46:12 -0600

    Added Chat Utils

--- src/main/kotlin/com/keurig/combatlogger/utils/Chat.kt
+++ src/main/kotlin/com/keurig/combatlogger/utils/Chat.kt
@@ -1,12 +1,19 @@
 package com.keurig.combatlogger.utils
 
+import org.bukkit.Bukkit
 import org.bukkit.ChatColor
 
 class Chat {
 
     companion object {
+        @JvmStatic
         fun color(message: String): String {
             return ChatColor.translateAlternateColorCodes('&', message)
+        }
+
+        @JvmStatic
+        fun log(message: Any?) {
+            return Bukkit.getConsoleSender().sendMessage(color(message as String))
         }
     }
 
